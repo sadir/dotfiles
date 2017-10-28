3a6e3014ac4f8527957f3c24828619dffa37a4a2
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2017-10-28 16:37:39 +0100

    Dynamically update GOROOT based on current golang version when bashrc is loaded

--- etc/profile.d/golang.sh
+++ etc/profile.d/golang.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
 export GOPATH=$HOME/code/go
-export GOROOT=$HOME/.asdf/installs/golang/1.9/go/
+GOV=$(asdf current golang | sed  's/ (set by .*)//g')
+export GOROOT=$HOME/.asdf/installs/golang/$GOV/go/
