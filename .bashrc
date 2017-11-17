397723ce8e4662d37a588496659f81251516d125
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2017-11-17 14:03:10 +0000

    Dont check in .extras.sh file for private exports

--- .bashrc
+++ .bashrc
@@ -1,4 +1,4 @@
-for file in $HOME/dotfiles/.{git-completion.sh, git-prompt.sh, bash_morgan, bash_aliases, .exports.sh, .paths.sh}; do
+for file in $HOME/.{git-completion.sh,git-prompt.sh,bash_morgan,bash_aliases,.exports.sh,.paths.sh,.extras.sh}; do
     [ -r "$file" ] && source "$file"
 done
 unset file
--- create_symlinks.sh
+++ create_symlinks.sh
@@ -135,9 +135,7 @@ print_success() {
 
 
 # finds all .dotfiles in this folder
-declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin .config/fish" # add in vim and the binaries
-
+declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx -not -name .gitignore | sed -e 's|//|/|' | sed -e 's|./.|.|')
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
