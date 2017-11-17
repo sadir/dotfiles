2aed79a0158f75e98dcfea0568aeb25ebc661fa3
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2017-11-17 12:26:09 +0000

    Brew script to install libraries

---
+++ brew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+brew update
+brew upgrade
+
+brew install awscli
+brew install coreutils
+brew install cmake
+brew install git
+brew install hub
+brew install moreutil
+brew install postgresql
+brew install ripgrep
+brew install tree
+brew install the_silver_searcher
+brew install vim --with-override-system-vi
+
+brew cleanup
