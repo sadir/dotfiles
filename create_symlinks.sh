57c30cef4fe26b3950b029207f5f224bc87de3d2
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2018-02-10 17:41:16 +0000

    Symlink .vim directory

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -136,6 +136,7 @@ print_success() {
 
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx -not -name .gitignore | sed -e 's|//|/|' | sed -e 's|./.|.|')
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
