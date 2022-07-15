5ade608d97b0017c264f304d446be5b3a9b1dc88
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2022-07-15 14:11:26 +0100

    Minor updates

--- .paths.sh
+++ .paths.sh
@@ -1,1 +1,1 @@
-export PATH="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/:$NODEROOT/.npm/bin/:./node_modules/.bin:$RUSTPATH:$GOPATH/bin:$PATH"
+export PATH="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/:$NODEROOT/.npm/bin/:./node_modules/.bin:$RUSTPATH:$GOPATH/bin:$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
