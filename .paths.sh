5644c44bce6475fd5ae56c45848bd1e5077f246c
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2025-01-09 10:29:29 +0000

    Only append to path
    Also make it prettier

--- .paths.sh
+++ .paths.sh
@@ -1,1 +1,6 @@
-export PATH="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/:$NODEROOT/.npm/bin/:./node_modules/.bin:/bin:$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin:/opt/homebrew/bin"
+export PATH="$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin/\
+:$NODEROOT/.npm/bin/\
+:./node_modules/.bin\
+:/bin\
+:/Applications/Visual Studio Code.app/Contents/Resources/app/bin\
+:/opt/homebrew/bin"
