2ac7fbf209235ee68f817d2f0e4a56c6f65e079c
Morgan Sadr-Hashemi <morgan.sadr@gmail.com>
2025-01-17 19:42:29 +0000

    Suppress Apple suggesting Zsh

--- .exports.sh
+++ .exports.sh
@@ -3,6 +3,10 @@
 export CODEROOT=$HOME/code
 export DOTFILES=$HOME/.dotfiles/
 export EDITOR=vim
+export ASDFINSTALLS=$HOME/.asdf/installs
+export ASDFROOT=$HOME/.asdf
 export ERL_AFLAGS="-kernel shell_history enabled"
 export NODEV=$(asdf current nodejs | sed  's/nodejs          \(.*\)          .*/\1/g')
 export NODEROOT=$ASDFINSTALLS/nodejs/$NODEV
+export XDG_CONFIG_HOME=$DOTFILES
+export BASH_SILENCE_DEPRECATION_WARNING=1
